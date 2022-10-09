1d93293c3fd3191fac3010702f5ff62a3c1c5093
Mieshkov Andrii <a.mieshkov.ip15@kpi.ua>
2022-10-09 15:40:47 +0200

    lab1

--- ProgAlg/Lab1/program/main.py
+++ ProgAlg/Lab1/program/main.py
@@ -3,7 +3,7 @@ from sorter import Sorter
 
 def main():
     sorter = Sorter("a.bin", "b.bin", "c.bin")
-    sorter.generate_file(1024**2, 1000000)
+    sorter.generate_file(1024**2, 1_000_000)
     sorter.sort()
     print(sorter)
 
